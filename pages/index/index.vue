99f937dbaf91fa17dccadb73f7bf256a55b7a5a6
xiaozhai1010 <xiaozhai1010@gmail.com>
2022-09-26 14:48:37 +0800

    no message

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,17 +1,29 @@
 <template>
-	<view class="content-clear-icon">
-		home page
+	<view class="content-clear-icon" @click="clickWord">
+		home page  {{num}}
+		<button @click="addOne">addone</button>
 	</view>
+	
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
+				num:0
 			}
 		},
 		methods: {
-
+			clickWord(){
+				uni.showToast({
+					icon:'none',
+					title:'你好',
+					duration:2000
+				})
+			},
+			addOne(){
+				this.num++
+			}
 		}
 	}
 </script>
